1a7b097744fcb749a0145f16a749ea8eacf87d22
akinoringo <wbraver@outlook.jp>
2022-07-23 15:46:20 +0900

    feat: bootstrap, jqueryの導入およびtemplateを用いたレイアウト表示の実装

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,11 +1,24 @@
 package controllers
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	// filesにfile pathを格納
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	// Mustを用いると、template.ParseFilesでエラーが生じた際にpanicのエラーを返す
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
+
 func top(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("app/views/templates/top.html")
-	t.Execute(w, nil)
+	// layoutも再度読み込ませないといけないっぽい
+	generateHTML(w, "Hello", "layout", "top")
 }
