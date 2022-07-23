45e6a910ddefbf0125b0b6aec525106d1d4c187e
akinoringo <wbraver@outlook.jp>
2022-07-23 10:52:23 +0900

    feat: todoの登録処理の実装

--- main.go
+++ main.go
@@ -8,18 +8,21 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.Password = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
+
+	user, _ := models.GetUser(2)
+
+	fmt.Println(user)
+
+	user.CreateTodo("First Todo")
+
 	/*
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@example.com"
-		u.Password = "testtest"
-		fmt.Println(u)
-
-		u.CreateUser()
-
-
-		u, _ := models.GetUser(1)
-
 		fmt.Println(u)
 
 		u.Name = "Test2"
@@ -32,4 +35,5 @@ func main() {
 		u, _ = models.GetUser(1)
 		fmt.Println(u)
 	*/
+
 }
